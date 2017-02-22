7ee18751fe788c9b2af171648fed15b8d9d50327
Samuli Kärkkäinen <samuli.karkkainen@iki.fi>
2017-02-22 20:54:23 +0200

    List map's road names

--- OSM2World/src/org/osm2world/core/map_data/object_info/ObjectInfo.java
+++ OSM2World/src/org/osm2world/core/map_data/object_info/ObjectInfo.java
@@ -2,11 +2,13 @@ package org.osm2world.core.map_data.object_info;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ObjectInfo {
 	public final String name;
 	public final ObjectType type;
 	public int importance;
+	public Set<Integer> houseNumbers = new TreeSet<>();
 	public Set<Point> points = new HashSet<>();
 	public Set<Point> borderCrossings = new HashSet<>(); // TODO
 	public Set<String> intersections = new HashSet<>(); // TODO
--- OSM2World/src/org/osm2world/core/map_data/object_info/ObjectInfoManager.java
+++ OSM2World/src/org/osm2world/core/map_data/object_info/ObjectInfoManager.java
@@ -3,6 +3,8 @@ package org.osm2world.core.map_data.object_info;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.openstreetmap.josm.plugins.graphview.core.data.Tag;
+import org.osm2world.core.map_data.data.MapNode;
 import org.osm2world.core.map_data.data.MapWaySegment;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,7 +17,6 @@ public class ObjectInfoManager {
 	public static void add(MapWaySegment line, ObjectType type) {
 		String name = line.getTags().getValue("name");
 		if (name == null || name.equals("")) {
-			System.out.println("WARNING: object has no name");
 			return;
 		}
 		ObjectInfo info = infos.get(name);
@@ -27,6 +28,19 @@ public class ObjectInfoManager {
 			System.out.println("WARNING: different object types for " + name);
 		}
 		info.points.add(new Point(line.getStartNode().getPos().x, line.getStartNode().getPos().z));
+		
+		for (Tag tag : line.getTags()) {
+			System.out.println("tag:" + tag.key + "=" + tag.value);
+		}
+		String houseNumber = line.getTags().getValue("addr:housenumber");
+		if (houseNumber != null && ! houseNumber.equals("")) {
+			try {
+				info.houseNumbers.add(Integer.valueOf(houseNumber));
+			} catch (Exception e) {
+				System.out.println(houseNumber);
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public static String getJsonLine() {
