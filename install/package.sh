7c540f9d58459531c489c693fb2ce771744ee0b4
Samuli Kärkkäinen <samuli.karkkainen@iki.fi>
2017-03-12 14:18:04 +0200

    Don't require a branch for releasing converter

--- install/package.sh
+++ install/package.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Tag release
-branch=$( git rev-parse --abbrev-ref HEAD )
-if [[ $branch != release/* ]]; then
-    echo "Current branch must be release/*, not $branch, maybe create one"
-    exit 1
-fi
+#branch=$( git rev-parse --abbrev-ref HEAD )
+#if [[ $branch != release/* ]]; then
+#    echo "Current branch must be release/*, not $branch, maybe create one"
+#    exit 1
+#fi
 time=$( date +%Y%m%d-%H%M%S )
 git tag package-$time
 
@@ -21,5 +21,5 @@ cp -alH ../OSM2World/build dist/OSM2World/
 cp -plH ../converter/*{py,sh} dist/
 cp -aH ../converter/py-lib dist/
 #cp -aH restart-pollers.sh cp-to-s3.sh dist/
-echo "$( date +'%Y-%m-%dT%H:%M:%S') $( git rev-parse --abbrev-ref HEAD ) $( git describe --tags )" >dist/VERSION.txt
+echo "$( date +'%Y-%m-%dT%H:%M:%S') $( git rev-parse --abbrev-ref HEAD ) $( git describe --tags ) $( git rev-parse --verify HEAD )" >dist/VERSION.txt
 #GZIP=-4 tar czf dist.tgz dist
